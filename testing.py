d994b9bdb65b566ea43d8e4b7beb172338b7c792
GoldenHatchet15 <raphsant20@gmail.com>
2024-02-23 10:25:02 -0400

    testpow

---
+++ testing.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import math
+
+def test_sqrt():
+    num = 25
+    assert math.sqrt(num) == 5
+    print("{}".format(math.sqrt(num)))
+
+# Uncomment the following line if you want to call the test function without using pytest
+test_sqrt()
+
+   
