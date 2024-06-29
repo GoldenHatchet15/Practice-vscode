a4cfd2ae1c28a0beb2852eadc6601c94be9d9be7
GoldenHatchet15 <raphsant20@gmail.com>
2024-06-29 06:30:34 -0400

    practice

--- Myfirstapi/populate_db.py
+++ Myfirstapi/populate_db.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+
 from db_setup import app, db, create_tables, Planet
 
 def populate_planet_data():
@@ -12,6 +13,7 @@ def populate_planet_data():
             Planet(name='Saturn', description='Saturn is the sixth planet from the Sun...', distance_from_sun='1.434 billion km', orbit_period='10759 days'),
             Planet(name='Uranus', description='Uranus is the seventh planet from the Sun...', distance_from_sun='2.871 billion km', orbit_period='30688 days'),
             Planet(name='Neptune', description='Neptune is the eighth planet from the Sun...', distance_from_sun='4.495 billion km', orbit_period='60182 days'),
+            Planet(name='Pluto', description='Pluto is a dwarf planet in the Kuiper belt...', distance_from_sun='5.906 billion km', orbit_period='90560 days'),
             Planet(name='Moon', description='The Moon is Earth\'s only permanent natural satellite...', distance_from_sun='384,400 km', orbit_period='27.3 days')
         ]
         db.session.bulk_save_objects(planets)
